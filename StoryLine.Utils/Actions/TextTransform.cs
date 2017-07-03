a290105705e12792589e2c62fc660999750bcf85
Diamond-Dragon <diamond_dragon@tut.by>
2017-07-03 23:40:43 +0300

    Text transformer updated

---
+++ StoryLine.Utils/Actions/TextTransform.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using StoryLine.Contracts;
+using StoryLine.Utils.Contracts;
+
+namespace StoryLine.Utils.Actions
+{
+    public class TextTransform : IActionBuilder
+    {
+        private ITextConverter _converter;
+        private Type _type;
+        private Func<string> _textProvider;
+
+        public TextTransform Source(Func<string> textProvider)
+        {
+            _textProvider = textProvider ?? throw new ArgumentNullException(nameof(textProvider));
+
+            return this;
+        }
+
+        public TextTransform Converter<T>()
+            where T : ITextConverter, new()
+        {
+            return Converter(new T());
+        }
+
+        public TextTransform Converter(ITextConverter converter)
+        {
+            _converter = converter ?? throw new ArgumentNullException(nameof(converter));
+
+            return this;
+        }
+
+        public TextTransform Target<T>()
+        {
+            return Target(typeof(T));
+        }
+
+        public TextTransform Target(Type type)
+        {
+            _type = type ?? throw new ArgumentNullException(nameof(type));
+
+            return this;
+        }
+
+        public IAction Build()
+        {
+            return new TextTransformAction(_type, _textProvider(), _converter);
+        }
+    }
+}
--- StoryLine.Utils/Actions/TextTransformAction.cs
+++ StoryLine.Utils/Actions/TextTransformAction.cs
@@ -5,16 +5,16 @@ using StoryLine.Utils.Contracts;
 
 namespace StoryLine.Utils.Actions
 {
-    public class TransformationAction : IAction
+    public class TextTransformAction : IAction
     {
         private readonly Type _type;
-        private readonly ITextProvider _provider;
+        private readonly string _text;
         private readonly ITextConverter _converter;
 
-        public TransformationAction(Type type, ITextProvider provider, ITextConverter converter)
+        public TextTransformAction(Type type, string text, ITextConverter converter)
         {
             _type = type ?? throw new ArgumentNullException(nameof(type));
-            _provider = provider ?? throw new ArgumentNullException(nameof(provider));
+            _text = text ?? throw new ArgumentNullException(nameof(text));
             _converter = converter ?? throw new ArgumentNullException(nameof(converter));
         }
 
@@ -23,13 +23,9 @@ namespace StoryLine.Utils.Actions
             if (actor == null)
                 throw new ArgumentNullException(nameof(actor));
 
-            var text = _provider.GetText();
-            if (string.IsNullOrEmpty(text))
-                throw new ExpectationException($"Failed to get text for artifact of type {_type}.");
-
-            var result = _converter.Convert(text, _type);
+            var result = _converter.Convert(_text, _type);
             if (result == null)
-                throw new ExpectationException($"Failed to convert text into artifact of type {_type}. Text to convert: {text}.");
+                throw new ExpectationException($"Failed to convert text into artifact of type {_type}. Text to convert: {_text}.");
 
             actor.Artifacts.Add(result);
         }
